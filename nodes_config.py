b5e2a5c4f32aa2306c79820eed5f9229c43fa122
Salem Olorundare <salemolorundare@gmail.com>
2024-04-13 12:50:19 +0100

    setup node

---
+++ nodes_config.py
@@ -0,0 +1,17 @@
+nodes = [
+    {
+        "address": "127.0.0.1",
+        "port": 74445,
+        "node_id": 1
+    },
+    {
+        "address": "127.0.0.1",
+        "port": 74446,
+        "node_id": 2
+    },
+    {
+        "address": "127.0.0.1",
+        "port": 74447,
+        "node_id": 3
+    }
+]
